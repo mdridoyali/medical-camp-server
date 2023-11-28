3d45a26ab5fee75b2469d27186ef374651a783ec
mdridoyali <mdridoyislamr@gmail.com>
2023-11-28 22:22:40 +0600

    api created for upcoming camps

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middle were
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://medicamp-603c8.web.app",
+      "https://medicamp-603c8.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w9fev91.mongodb.net/?retryWrites=true&w=majority`;
@@ -37,6 +47,7 @@ async function run() {
     const usersCollection = client.db("mediCampDB").collection("users");
     const registeredCampCollection = client.db("mediCampDB").collection("registeredCamp");
     const paymentHistoryCollection = client.db("mediCampDB").collection("payments");
+    const upcomingCampCollection = client.db("mediCampDB").collection("upcoming-camps");
 
 
     // user related api
@@ -253,6 +264,26 @@ async function run() {
       // const deleteResult = await registeredCampCollection.deleteMany(query)
       res.send({ paymentResult})
     })
+
+
+
+    // add upcoming camp related api
+
+    app.post('/upcoming-camp', async (req, res) => {
+      const camp = req.body
+      console.log(camp)
+      const result = await upcomingCampCollection.insertOne(camp);
+      res.send(result)
+    })
+   
+    app.get('/upcoming-camp', async (req, res) => {
+      const result = await upcomingCampCollection.find().toArray();
+      res.send(result)
+    })
+   
+
+
+
 
 
 
